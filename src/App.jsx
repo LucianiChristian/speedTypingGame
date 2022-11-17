705f71dcc6047db80ec0a4bd5020e42ef5b38075
Christian Luciani <luciani_christian@yahoo.com>
2022-11-17 09:30:51 -0600

    Create HTML and initial CSS.

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,13 @@ import React from 'react'
 import './App.css'
 
 export default function App() {
-  
+    return (
+        <div>
+            <h1>How fast do you type?</h1>
+            <textarea />
+            <h4>Time remaining: </h4>
+            <button>Start</button>
+            <h4>Word count: </h4>
+        </div>
+    )
 }
