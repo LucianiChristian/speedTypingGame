48b15a1703d865f9e84a9076aea7b29d01a894fc
Christian Luciani <luciani_christian@yahoo.com>
2022-11-21 08:52:02 -0600

    Create text state

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import './App.css'
 
 export default function App() {
+    const [text, setText] = React.useState('');
+
+    function handleChange(e) {
+        setText(e.target.value);
+    }
+
     return (
         <div className="container">
             <h1>How fast do you type?</h1>
-            <textarea />
+            <textarea value={text} onChange={handleChange}/>
             <section className="info">
                 <h4>Time remaining: </h4>
                 <h4>Word count: </h4>
