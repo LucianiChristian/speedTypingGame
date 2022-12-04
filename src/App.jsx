99030bbe5e52d04f3b41b3336a89e0618d3dd1d2
Christian Luciani <luciani_christian@yahoo.com>
2022-12-04 16:56:28 -0600

    Add state for timer, wordCount, and gameState

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,36 @@ import './App.css'
 
 export default function App() {
     const [text, setText] = React.useState('');
+    const [time, setTime] = React.useState(5);
+    const [isGameActive, setIsGameActive] = React.useState(false);
+    const [wordCount, setWordCount] = React.useState(0);
+
+    React.useEffect(() => {
+        if(isGameActive && time > 0) {
+            secondElapse();
+        }
+        else if(time === 0) {
+            toggleGame();
+            setWordCount(getWordCount(text));
+        }
+    }, [time, isGameActive])
 
     function handleChange(e) {
         setText(e.target.value);
     }
 
+    function toggleGame() {
+        setIsGameActive(prevIsGameActive => !prevIsGameActive);
+    }
+
+    function secondElapse() {
+        setTimeout(() => {
+            setTime(prevTime => prevTime - 1);
+        }, 1000);
+    }
+
     function getWordCount(str) {
-        alert(str.trim() ? str.trim().split(/\s+/).length : 0);
+        return str.trim() ? str.trim().split(/\s+/).length : 0;
     }
 
     return (
@@ -17,9 +40,9 @@ export default function App() {
             <h1>How fast do you type?</h1>
             <textarea value={text} onChange={handleChange}/>
             <section className="info">
-                <h4>Time remaining: </h4>
-                <h4 onClick={() => getWordCount(text)}>Word count: </h4>
-                <button>Start</button>
+                <h4>Time remaining: {time}</h4>
+                <h4>Word count: {wordCount}</h4>
+                <button onClick={toggleGame}>Start</button>
             </section>
         </div>
     )
