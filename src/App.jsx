3eeef82ebb2282ef3958e28d7d1a742fcb148a33
Christian Luciani <luciani_christian@yahoo.com>
2022-12-04 10:15:26 -0600

    Create textArea state and corresponding input processing

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,17 @@ export default function App() {
         setText(e.target.value);
     }
 
+    function getWordCount(str) {
+        alert(str.trim() ? str.trim().split(/\s+/).length : 0);
+    }
+
     return (
         <div className="container">
             <h1>How fast do you type?</h1>
             <textarea value={text} onChange={handleChange}/>
             <section className="info">
                 <h4>Time remaining: </h4>
-                <h4>Word count: </h4>
+                <h4 onClick={() => getWordCount(text)}>Word count: </h4>
                 <button>Start</button>
             </section>
         </div>
